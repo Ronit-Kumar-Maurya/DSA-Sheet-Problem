4e0a3a6babc5928ebfec0c0ab6caaa24556fa5c9
Ronit-Kumar-Maurya <ronitmaurya123@gmail.com>
2026-08-03 19:42:03 +0530

    Time: 0 ms (100%), Space: 43.4 MB (61.69%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,26 +1,49 @@
 class Solution {
     public void sortColors(int[] nums) {
-        int count0 = 0;
-        int count1 = 0;
-        int count2 = 0;
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == 0)
-                count0++;
-            else if (nums[i] == 1)
-                count1++;
-            else
-                count2++;
-        }
+        // int count0 = 0;
+        // int count1 = 0;
+        // int count2 = 0;
+        // for (int i = 0; i < nums.length; i++) {
+        //     if (nums[i] == 0)
+        //         count0++;
+        //     else if (nums[i] == 1)
+        //         count1++;
+        //     else
+        //         count2++;
+        // }
 
-        int idx = 0;
-        for (int i = 0; i < count0; i++) {
-            nums[idx++] = 0;
-        }
-        for (int i = 0; i < count1; i++) {
-            nums[idx++] = 1;
-        }
-        for (int i = 0; i < count2; i++) {
-            nums[idx++] = 2;
+        // int idx = 0;
+        // for (int i = 0; i < count0; i++) {
+        //     nums[idx++] = 0;
+        // }
+        // for (int i = 0; i < count1; i++) {
+        //     nums[idx++] = 1;
+        // }
+        // for (int i = 0; i < count2; i++) {
+        //     nums[idx++] = 2;
+        // }
+
+        //DNF algorithm
+
+        int low =0;
+        int mid =0;
+        int high = nums.length-1;
+
+        while(mid<=high){
+            if(nums[mid]==0){
+                int temp = nums[low];
+                nums[low] = nums[mid];
+                nums[mid] = temp;
+                mid++;
+                low++;
+            }else if(nums[mid]==1){
+                mid++;
+            }else{
+                int temp = nums[mid];
+                nums[mid] = nums[high];
+                nums[high] = temp;
+                high--;
+            }
         }
     }
 }
