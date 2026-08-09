92dcbf830def4ea2e8a507a15c26e5143bce26bb
Ronit-Kumar-Maurya <ronitmaurya123@gmail.com>
2026-08-09 18:55:56 +0530

    Time: 2 ms (99.32%), Space: 44.2 MB (83.96%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -0,0 +1,33 @@
+class Solution {
+
+    boolean isAlphaNum(char ch){
+            if((ch>='0'&&ch<='9')||(ch>='a'&&ch<='z')||(ch>='A'&&ch<='Z')){
+                return true;
+            }
+            return false;
+        };
+
+
+    public boolean isPalindrome(String s) {
+        int n = s.length();
+        int start =0;
+        int end = n-1;
+
+        while(start<=end){
+            if(!isAlphaNum(s.charAt(start))){
+                start++; continue;
+            }
+            if(!isAlphaNum(s.charAt(end))){
+                end--; continue;
+            }
+
+            if (Character.toLowerCase(s.charAt(start)) !=
+                Character.toLowerCase(s.charAt(end))) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
+    }
+}
