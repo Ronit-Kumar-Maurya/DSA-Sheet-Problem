57f7ad22973ba321073eb82815c188c23ba9f2ba
Ronit-Kumar-Maurya <ronitmaurya123@gmail.com>
2026-09-06 17:47:54 +0530

    Time: 2 ms (95.13%), Space: 45.5 MB (91.97%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,24 +1,22 @@
 class Solution {
-    public void combinSum(int[] arr, int i, List<Integer> combin, List<List<Integer>> ans, int tar){
-        if(i==arr.length||tar<0){
-            return;
-        }
-        if(tar==0){
+
+    public void combinSum(int[] arr, int i, List<Integer> combin, List<List<Integer>> ans, int target){
+        if(i==arr.length || target<0) return;
+        if(target==0){
             ans.add(new ArrayList<> (combin));
             return;
         }
-
         combin.add(arr[i]);
-        combinSum(arr,i,combin,ans,tar-arr[i]);
-        combin.remove(combin.size() - 1);
-        combinSum(arr,i+1,combin,ans,tar);
+        combinSum(arr, i, combin, ans, target-arr[i]);
+        combin.remove(combin.size()-1);
+        combinSum(arr, i+1, combin, ans, target);
     }
 
     public List<List<Integer>> combinationSum(int[] arr, int target) {
-        List<List<Integer>> ans= new ArrayList<>();
+        List<List<Integer>> ans = new ArrayList<>();
         List<Integer> combin = new ArrayList<>();
 
-        combinSum(arr,0,combin,ans,target);
+        combinSum(arr, 0, combin, ans, target);
         return ans;
     }
 }
