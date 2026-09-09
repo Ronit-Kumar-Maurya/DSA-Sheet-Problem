1081350ffeb7604f2dfcb95bb73bd859f699ec1a
Ronit-Kumar-Maurya <ronitmaurya123@gmail.com>
2026-09-09 09:29:56 +0530

    Time: 206 ms (73.54%), Space: 42.6 MB (94.56%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.java
+++ 0037-sudoku-solver/0037-sudoku-solver.java
@@ -2,44 +2,33 @@ class Solution {
 
     public boolean isSafe(char[][] board, int row, int col, int dig){
         char num = (char)(dig + '0');
-
         // Column check
         for(int i=0; i<9; i++){
             if(board[i][col] == num){
                 return false;
             }
         }
-
         // Row check
         for(int j=0; j<9; j++){
             if(board[row][j] == num){
                 return false;
             }
         }
-
         // 3x3 box check
-        int startRow = (row/3)*3;
-        int startCol = (col/3)*3;
-
-        for(int i=startRow; i<startRow+3; i++){
-            for(int j=startCol; j<startCol+3; j++){
-                if(board[i][j] == num){
-                    return false;
-                }
+        int sR = (row/3)*3;
+        int sC = (col/3)*3;
+        for(int i=sR; i<sR+3; i++){
+            for(int j=sC; j<sC+3; j++){
+                if(board[i][j] == num) return false;
             }
         }
-
         return true;
     }
 
     public boolean solver(char[][] board, int row, int col){
-        if(row==9){
-            return true;
-        }
+        if(row==9) return true;
 
-        if(col==9){
-            return solver(board,row+1,0);
-        }
+        if(col==9) return solver(board,row+1,0);
 
         if(board[row][col]=='.'){
             for(int dig=1; dig<=9; dig++){
