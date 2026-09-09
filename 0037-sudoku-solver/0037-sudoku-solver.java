8a8884db9decd755d811be47f0a2a792cca0a1f0
Ronit-Kumar-Maurya <ronitmaurya123@gmail.com>
2026-09-09 09:26:34 +0530

    Time: 211 ms (72.25%), Space: 42.7 MB (88.42%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.java
+++ 0037-sudoku-solver/0037-sudoku-solver.java
@@ -1,56 +1,63 @@
 class Solution {
 
-    public boolean isSafe(char[][] board, int row, int col, int i){
-        char num = (char)(i + '0');
+    public boolean isSafe(char[][] board, int row, int col, int dig){
+        char num = (char)(dig + '0');
+
         // Column check
-        for(int k = 0; k < 9; k++){
-            if(board[k][col] == num){
+        for(int i=0; i<9; i++){
+            if(board[i][col] == num){
                 return false;
             }
         }
+
         // Row check
-        for(int k = 0; k < 9; k++){
-            if(board[row][k] == num){
+        for(int j=0; j<9; j++){
+            if(board[row][j] == num){
                 return false;
             }
         }
+
         // 3x3 box check
-        int startRow = (row / 3) * 3;
-        int startCol = (col / 3) * 3;
-        for(int k = startRow; k < startRow + 3; k++){
-            for(int m = startCol; m < startCol + 3; m++){
-                if(board[k][m] == num){
+        int startRow = (row/3)*3;
+        int startCol = (col/3)*3;
+
+        for(int i=startRow; i<startRow+3; i++){
+            for(int j=startCol; j<startCol+3; j++){
+                if(board[i][j] == num){
                     return false;
                 }
             }
         }
+
         return true;
     }
 
-    public boolean solver(char[][] board, int row, int col) {
-        if (row == 9) {
+    public boolean solver(char[][] board, int row, int col){
+        if(row==9){
             return true;
         }
-        if (col == 9) {
-            return solver(board, row + 1, 0);
+
+        if(col==9){
+            return solver(board,row+1,0);
         }
-        if (board[row][col] == '.') {
-            for (int i = 1; i <= 9; i++) {
-                if (isSafe(board, row, col, i)) {
-                    board[row][col] = (char)(i + '0');
-                    if (solver(board, row, col + 1)) {
+
+        if(board[row][col]=='.'){
+            for(int dig=1; dig<=9; dig++){
+                if(isSafe(board,row,col,dig)){
+                    board[row][col]=(char)(dig+'0');
+                    if(solver(board,row,col+1)){
                         return true;
                     }
-                    board[row][col] = '.';
+                    board[row][col]='.';
                 }
             }
             return false;
-        } else {
-            return solver(board, row, col + 1);
+        }else{
+            return solver(board,row,col+1);
         }
     }
 
-    public void solveSudoku(char[][] board) {
-        solver(board, 0, 0);
+    public void solveSudoku(char[][] board){
+        solver(board,0,0);
     }
 }
